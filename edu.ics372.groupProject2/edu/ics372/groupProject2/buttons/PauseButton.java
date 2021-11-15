fa76ec212290160b69a53bd49352aa627932a156
SweetNaterTater <41494817+SweetNaterTater@users.noreply.github.com>
2021-11-15 15:10:29 -0600

    Update PauseButton.java

--- edu.ics372.groupProject2/edu/ics372/groupProject2/buttons/PauseButton.java
+++ edu.ics372.groupProject2/edu/ics372/groupProject2/buttons/PauseButton.java
@@ -1,19 +1,28 @@
 package edu.ics372.groupProject2.buttons;
 
+import edu.ics372.groupProject2.states.PlayerContext;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 
+/**
+ * The player on request is made using an instance of this class
+ * 
+ * @author Nathan Lantaigne-Goetsch
+ *
+ */
 public class PauseButton extends GUIButton implements EventHandler<ActionEvent> {
-
+	/**
+	 * Create the button with the proper display
+	 * 
+	 * @param string the text to be put
+	 */
 	public PauseButton(String string) {
 		super(string);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void handle(ActionEvent arg0) {
-		// TODO Auto-generated method stub
-
+		PlayerContext.getInstance().onPauseShowRequest();
 	}
 
 }
