5ae8141f653a764b4b5acd85cf602e2f5d66b44b
SweetNaterTater <41494817+SweetNaterTater@users.noreply.github.com>
2021-11-15 15:08:54 -0600

    Update and rename OffButton.java to PlayerOffButton.java

---
+++ edu.ics372.groupProject2/edu/ics372/groupProject2/buttons/PlayerOffButton.java
@@ -0,0 +1,28 @@
+package edu.ics372.groupproject2.buttons;
+
+import edu.ics372.groupproject2.states.PlayerContext;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+/**
+ * The player on request is made using an instance of this class
+ * 
+ * @author Nathan Lantaigne-Goetsch
+ *
+ */
+public class PlayerOffButton extends GUIButton implements EventHandler<ActionEvent> {
+	/**
+	 * Create the button with the proper display
+	 * 
+	 * @param string the text to be put
+	 */
+	public PlayerOffButton(String string) {
+		super(string);
+	}
+
+	@Override
+	public void handle(ActionEvent arg0) {
+		PlayerContext.getInstance().onOffRequest();
+	}
+
+}
