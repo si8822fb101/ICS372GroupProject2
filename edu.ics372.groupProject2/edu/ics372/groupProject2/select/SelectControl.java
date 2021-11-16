3aa274b9a831be5ada5166573b6a7d17bc96b2dc
SweetNaterTater <41494817+SweetNaterTater@users.noreply.github.com>
2021-11-15 22:22:08 -0600

    Create SelectControl.java

---
+++ edu.ics372.groupProject2/edu/ics372/groupProject2/select/SelectControl.java
@@ -0,0 +1,26 @@
+package edu.ics372.groupProject2.select;
+
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.control.ListView;
+
+//Create a singleton class called ShowList with a list of Show objects.
+//You need to figure out what a Show object should contain,
+//The list could be an ObservableList.
+//You could use the following code to select a show.
+
+public class SelectControl extends ListView<Show> {
+	public SelectControl() {
+		super(ShowList.getInstance().getShows()); // gets the ObservableList
+		setOnMouseClicked(new EventHandler<Event>() {
+			@Override
+			public void handle(Event arg0) {
+				System.out.println("clicked on event " + getSelectionModel().getSelectedItem());
+
+				Show show = (Show) getSelectionModel().getSelectedItem();
+				// code to use the Show object
+			}
+		});
+
+	}
+}
