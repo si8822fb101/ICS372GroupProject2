09c51151f5e601b039a9f48c4afaef314d0ec736
Qaalib Farah <31248132+qaalib101@users.noreply.github.com>
2021-11-16 18:49:24 -0600

    worked on transition

--- edu.ics372.groupProject2/edu/ics372/groupProject2/states/SelectState.java
+++ edu.ics372.groupProject2/edu/ics372/groupProject2/states/SelectState.java
@@ -1,17 +1,61 @@
 package edu.ics372.groupProject2.states;
 
+/**
+ * 
+ * Represents the idle; show selected state
+ * 
+ * @author Qaalib Farah
+ *
+ */
 public class SelectState extends PlayerState {
+
+	private static SelectState instance;
+
+	/**
+	 * Private constructor for the singleton pattern
+	 */
+	private SelectState() {
+		instance = this;
+	}
+
+	/**
+	 * For the singleton pattern
+	 * 
+	 * @return the object
+	 */
+	public static SelectState getInstance() {
+		if (instance == null) {
+			instance = new SelectState();
+		}
+		return instance;
+	}
+
+	public void onOffRequest() {
+		PlayerContext.getInstance().changeState(PlayerOffState.getInstance());
+	}
+
+	/*
+	 * Handle play show request
+	 */
+	@Override
+	public void onPlayShowRequest() {
+		PlayerContext.getInstance().changeState(PlayState.getInstance());
+	}
+
+	/*
+	 * Process clock ticks Generates the timer runs out event
+	 */
+	public void onTimerRunsOut() {
+		PlayerContext.getInstance().changeState(ScreenSaverState.getInstance());
+	}
 
 	@Override
 	public void enter() {
-		// TODO Auto-generated method stub
-
+		PlayerContext.getInstance().showSelectedShow();
 	}
 
 	@Override
 	public void leave() {
-		// TODO Auto-generated method stub
 
 	}
-
 }
