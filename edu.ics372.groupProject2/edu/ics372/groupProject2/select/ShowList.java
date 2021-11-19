d526b6e3498c5a05964484b21dca27aa132dc0d9
SweetNaterTater <41494817+SweetNaterTater@users.noreply.github.com>
2021-11-18 21:50:58 -0600

    Update ShowList.java

--- edu.ics372.groupProject2/edu/ics372/groupProject2/select/ShowList.java
+++ edu.ics372.groupProject2/edu/ics372/groupProject2/select/ShowList.java
@@ -2,9 +2,10 @@ package edu.ics372.groupProject2.select;
 
 import javafx.collections.ObservableList;
 
-public class ShowList {
+public class ShowList extends SelectControl {
 	private static ShowList instance;
 	private ObservableList<Show> shows;
+	private Show selectedShow;
 
 	/**
 	 * Private for the singleton pattern
@@ -26,6 +27,14 @@ public class ShowList {
 
 	public ObservableList<Show> getShows() {
 		return this.shows;
+	}
+
+	public Show getShow() {
+		return this.selectedShow;
+	}
+
+	public void setShow(Show show) {
+		this.selectedShow = show;
 	}
 
 }
