cb274cba342ac9e5ba65168eb04f7a7fdd9015d9
SweetNaterTater <41494817+SweetNaterTater@users.noreply.github.com>
2021-11-30 10:08:30 -0600

    Add files via upload

---
+++ edu.ics372.groupProject2/edu/ics372/groupProject2/states/PauseState.java
@@ -0,0 +1,88 @@
+package edu.ics372.groupProject2.states;
+/*
+* @author Nathan Lantainge-Goetsch
+ * @Copyright (c) 2021
+ 
+ * Redistribution and use with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - the use is for academic purpose only
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   - Neither the name of Brahma Dathan or Sarnath Ramnath
+ *     may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * The authors do not make any claims regarding the correctness of the code in this module
+ * and are not responsible for any loss or damage resulting from its use.  
+ */
+
+import edu.ics372.groupProject2.timer.Notifiable;
+import edu.ics372.groupProject2.timer.Timer;
+
+/**
+ * Represents the pause show state
+ *
+ */
+public class PauseState extends PlayerState implements Notifiable { // Notifiable implementation?
+	private static PauseState instance;
+	private Timer timer;
+
+	/**
+	 * Private constructor for the singleton pattern
+	 */
+	private PauseState() {
+	}
+
+	/**
+	 * For the singleton pattern
+	 * 
+	 * @return the object
+	 */
+	public static PauseState getInstance() {
+		if (instance == null) {
+			instance = new PauseState();
+		}
+		return instance;
+	}
+
+	/*
+	 * Handle player off event
+	 */
+	@Override
+	public void onOffRequest() {
+		PlayerContext.getInstance().changeState(PlayerOffState.getInstance());
+	}
+
+	@Override
+	public void onStopShowRequest() {
+		PlayerContext.getInstance().changeState(CompleteState.getInstance());
+	}
+
+	@Override
+	public void onTimerTicked(int timeLeft) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/*
+	 * Handle play show event
+	 */
+	@Override
+	public void onPlayShowRequest() {
+		PlayerContext.getInstance().changeState(PlayState.getInstance());
+	}
+
+	@Override
+	public void enter() {
+		PlayerContext.getInstance().showPausedShow();
+		PlayerContext.getInstance().showTimeLeft(timer.getTimeValue());
+	}
+
+	@Override
+	public void leave() {
+		PlayerContext.getInstance().showStoppedShow();
+	}
+
+}
---
+++ edu.ics372.groupProject2/edu/ics372/groupProject2/states/PlayerOffState.java
@@ -0,0 +1,70 @@
+package edu.ics372.groupProject2.states;
+
+/**
+ * 
+ * @author Nathan Lantainge-Goetsch
+ * @Copyright (c) 2021
+ 
+ * Redistribution and use with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - the use is for academic purpose only
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   - Neither the name of Brahma Dathan or Sarnath Ramnath
+ *     may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * The authors do not make any claims regarding the correctness of the code in this module
+ * and are not responsible for any loss or damage resulting from its use.  
+ */
+
+/**
+ * Represents the door closed state
+ *
+ */
+public class PlayerOffState extends PlayerState {
+
+	private static PlayerOffState instance;
+
+	/**
+	 * Private constructor for the singleton pattern
+	 */
+	private PlayerOffState() {
+	}
+
+	/**
+	 * For the singleton pattern
+	 * 
+	 * @return the object
+	 */
+	public static PlayerOffState getInstance() {
+		if (instance == null) {
+			instance = new PlayerOffState();
+		}
+		return instance;
+	}
+
+	/**
+	 * Handle player on event
+	 */
+	@Override
+	public void onOnRequest() {
+		PlayerContext.getInstance().changeState(PlayerOnState.getInstance());
+	}
+
+	/**
+	 * initialize the state
+	 * 
+	 */
+	@Override
+	public void enter() {
+		PlayerContext.getInstance().showPlayerOff();
+	}
+
+	@Override
+	public void leave() {
+		PlayerContext.getInstance().showPlayerOn();
+	}
+}
